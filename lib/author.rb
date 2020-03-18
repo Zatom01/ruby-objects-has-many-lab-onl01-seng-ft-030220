135f4ed6cbc83fa469e06a4ce6b3816c4468b86f
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-18 04:59:24 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -31,9 +31,10 @@ class Author
     @@post_count+=1 
   end 
   
-  def post 
-    Post.all.select {|author| author.author=self}
-  end
+  def posts
+    Author.all.collect do |post|
+      post.author==self 
+  end 
   
  
   
