60f6d72c48c3a9eb3cc20a9f31d0165629384a9c
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-18 04:12:19 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,41 @@
+class Post 
+  
+  @@all=[]
+  
+  attr_accessor :title, :author
+  
+  def initialize(title)
+    @title=title 
+    @@all << self 
+   
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def author_name
+    if @author==nil 
+      return nil 
+    else 
+      @author.name 
+    end 
+  end 
+  
+  def posts
+    @@all.collect do |post|
+      post.author=self 
+    end 
+  end 
+  
+   
+
+    
+
+  
+
+  
+  
+  
+  
+end 
